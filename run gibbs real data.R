b7515e202f50253eca343aa187d9d45587c13dba
drvalle@ufl.edu <drvalle@ufl.edu>
2017-11-10 15:55:47 -0500

    Commit 2
    Edited code to run the model on the real dataset

--- run gibbs real data.R
+++ run gibbs real data.R
@@ -10,14 +10,32 @@ source('gibbs_main.R')
 setwd('U:/independent studies/kaplan amazon hydrology/hydrology data')
 tmp=read.csv('final edited.csv',as.is=T)
 
-dat=read.csv('fake data.csv',as.is=T)
-dat1=data.frame(y=dat[,1],mes=dat$mes,tempo=dat$tempo)
-model=gibbs_main(dat1)
+#eliminate lagged variables
+ind=grep('_lag',colnames(tmp)); colnames(tmp)[ind]
+tmp1=tmp[,-ind]
 
-setwd('U:/independent studies/kaplan amazon hydrology/hydrology data/stationarity/results')
-nomes=colnames(dat)[uuu]
-nomes1=paste(c('betas_','outros_'),nomes,'.csv',sep='')
-seq1=seq(from=ngibbs*0.5,to=ngibbs,length.out=1000)
-write.csv(vec.betas[seq1,],nomes1[1],row.names=F)
-write.csv(vec.outros[seq1,],nomes1[2],row.names=F)
+#remove water level data after impact (i.e., after 1984)
+ind=grep('w',colnames(tmp1)); colnames(tmp1)[ind]
+cond=tmp1$ano >= 1984
+tmp1[cond,ind]=NA
+image(data.matrix(tmp1))
+
+ngibbs=10000
+a1.sigma2=b1.sigma2=0.1
+ind=which(colnames(tmp1)%in%c('ano','mes'))
+nomes=colnames(tmp1)[-ind]; nomes
+
+for (i in 1:length(nomes)){
+  dat=data.frame(y=tmp1[,nomes[i]],mes=tmp1$mes,ano=tmp1$ano)
+  model=gibbs_main(dat=dat,ngibbs=ngibbs,a1.sigma2=a1.sigma2,b1.sigma2=b1.sigma2)
+  
+  #output results
+  setwd('U:/independent studies/kaplan amazon hydrology/gibbs stationarity/results')
+  nomes1=paste(c('betas_','outros_'),nomes[i],'.csv',sep='')
+  seq1=seq(from=ngibbs*0.5,to=ngibbs,length.out=1000)
+  write.csv(model$betas[seq1,],nomes1[1],row.names=F)
+  write.csv(model$outros[seq1,],nomes1[2],row.names=F)
 }
+
+
+
